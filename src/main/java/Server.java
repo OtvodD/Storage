ad506abc5092db0a738ca22eda01fb0e6085c427
Dima <Lebocski@yandex.ru>
2021-06-21 23:55:09 +0700

    Lesson1

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,22 @@
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+    public Server() {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        try (ServerSocket server = new ServerSocket(5678)){
+            System.out.println("Server started");
+            while (true) {
+                service.execute(new ClientHandler(server.accept()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
 }
+
